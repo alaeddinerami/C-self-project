0617c545f67db0b828b00ccb7da3bc553dc4cb82
alaeddine <alaeddinerami7@gmail.com>
2023-09-17 22:52:33 +0100

    calcul

---
+++ calcultor/calcul.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+int main()
+{   while (1)
+{
+    char op;
+    double num1,num2,res;
+    int i;           
+    printf("\nEnter an operator (+, -, *, /, ^) if want to exit press x: ");
+    scanf("%c", &op);
+    
+    if (op == 'x')
+    {
+        return 1; 
+    }
+    printf("Enter two first and second operand: ");
+    scanf("%lf %lf", &num1, &num2);    
+    while(getchar() != '\n');
+    switch (op)
+    {
+    case '+':
+         res = num1 + num2;
+         printf("%.1lf + %.1lf = %.1lf",num1,num2,res);
+        break;
+
+    case '-':
+         res = num1 - num2;
+         printf("%.1lf - %.1lf = %.1lf",num1,num2,res);
+        break;    
+    
+    case '*':
+         res = num1 * num2;
+         printf("%.1lf * %.1lf = %.1lf",num1,num2,res);
+        break;
+    
+    case '/':
+        if (num2 == 0)
+        {
+            printf("inposible \n");
+            return 1;
+        }
+        
+         res = num1 / num2;
+         printf("%.1lf / %.1lf = %.1lf",num1,num2,res);
+        break;
+
+    case '^':
+        res = 1;
+        for ( i = 0; i < num2; i++)
+        {
+            res *= num1;
+        }
+        printf("%.1lf ^ %.1lf = %.1lf",num1,num2,res);
+        break;
+    }
+}    
+return 0;    
+    
+}
+
